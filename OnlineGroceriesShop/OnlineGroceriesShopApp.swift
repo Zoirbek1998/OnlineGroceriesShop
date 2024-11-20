67991e5f29449a7968fd856f51f2e3afce9b4cf6
Zoirbek1998 <zoirbekmuxtorov98@gmail.com>
2024-11-20 19:32:55 +0500

    Initial Commit

---
+++ OnlineGroceriesShop/OnlineGroceriesShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGroceriesShopApp.swift
+//  OnlineGroceriesShop
+//
+//  Created by macstore.uz on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct OnlineGroceriesShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
