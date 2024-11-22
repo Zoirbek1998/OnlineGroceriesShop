e657dc3be55f5bb4255f6d0f29423b2d3a97ac6a
Zoirbek <@m.zoirbek>
2024-11-22 17:56:10 +0500

    login

---
+++ OnlineGroceriesShop/View/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  OnlineGroceriesShop
+//
+//  Created by Zoirbek Muxtorov on 22/11/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
